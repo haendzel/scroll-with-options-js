e959989e94dcddfd9343292148b7239e978d0b36
Filip Handzel <f.handzel@gmail.com>
2020-01-31 21:31:55 +0100

    init project

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+const btns = [...(document.querySelectorAll('header button'))];
+const sections = [...(document.querySelectorAll('article div'))];
+
+class Slide {
+    constructor() {
+        for(let i = 0; i < 10; i++) {
+            console.log('Monkey');
+        }
+    }
+}
+
+const slide = new Slide();
+
